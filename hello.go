f605de2fcf70e326c79fcbffd8ea050a2961e4db
fanialfi <fanialfirdaus@gmail.com>
2023-07-13 22:06:23 +0700

    inisialisasi project go
    inisialisasi project go dengan menggunakan perintah `go mod init <nama-project>`

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main // nama package
+
+import "fmt"
+
+func main()  {
+  fmt.Println("Hello World")
+}
+
+// untuk menginisialisasi project baru dengan menggunakan go modules gunakan command `go mod init <nama-project>`
